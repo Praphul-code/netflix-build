772c20d5e0c60a6fc03501cf1e480f4c37256fa2
Praphul-code <praphulporwal1194@gmail.com>
2023-01-01 22:30:42 +0530

    navbar1.0

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkau7G7djbC-Wl7MntUVYQGV7W4sj2IzE",
+  authDomain: "netfix-clone-7e8c8.firebaseapp.com",
+  projectId: "netfix-clone-7e8c8",
+  storageBucket: "netfix-clone-7e8c8.appspot.com",
+  messagingSenderId: "77331346144",
+  appId: "1:77331346144:web:1451359925fb03f7205685",
+  measurementId: "G-MCY1CMRVGG"
+};
